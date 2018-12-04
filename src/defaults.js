ae3c4a9b74b68a5a7c53e42be7e81f019fd81a20
Maksim Ivanov <satansdeer@gmail.com>
2018-12-04 11:54:24 +0100

    Switch to legacy-decorators

--- src/defaults.js
+++ src/defaults.js
@@ -1,46 +1,46 @@
 // Default mappings between function arguments and their corresponding
 // .pot keys
 export const GETTEXT_FUNC_ARGS_MAP = {
-  gettext: ['msgid'],
-  dgettext: [null, 'msgid'],
-  ngettext: ['msgid', 'msgid_plural'],
-  dngettext: [null, 'msgid', 'msgid_plural'],
-  pgettext: ['msgctxt', 'msgid'],
-  dpgettext: [null, 'msgctxt', 'msgid'],
-  npgettext: ['msgctxt', 'msgid', 'msgid_plural'],
-  dnpgettext: [null, 'msgctxt', 'msgid', 'msgid_plural'],
+  gettext: ["msgid"],
+  dgettext: [null, "msgid"],
+  ngettext: ["msgid", "msgid_plural"],
+  dngettext: [null, "msgid", "msgid_plural"],
+  pgettext: ["msgctxt", "msgid"],
+  dpgettext: [null, "msgctxt", "msgid"],
+  npgettext: ["msgctxt", "msgid", "msgid_plural"],
+  dnpgettext: [null, "msgctxt", "msgid", "msgid_plural"]
 };
 
 // Default mappings between gettext components' attributes and their
 // corresponding .pot keys
 export const GETTEXT_COMPONENT_PROPS_MAP = {
   GetText: {
-    message: 'msgid',
-    messagePlural: 'msgid_plural',
-    context: 'msgctxt',
-    comment: 'comment',
-  },
+    message: "msgid",
+    messagePlural: "msgid_plural",
+    context: "msgctxt",
+    comment: "comment"
+  }
 };
 
 // Default parsing options
 export const BABEL_PARSING_OPTS = {
-  sourceType: 'module',
+  sourceType: "module",
   plugins: [
-    'jsx',
-    'asyncFunctions',
-    'classConstructorCall',
-    'doExpressions',
-    'trailingFunctionCommas',
-    'objectRestSpread',
-    'decorators',
-    'classProperties',
-    'exportExtensions',
-    'exportDefaultFrom',
-    'exportNamespaceFrom',
-    'exponentiationOperator',
-    'asyncGenerators',
-    'functionBind',
-    'functionSent',
-    'dynamicImport',
-  ],
+    "jsx",
+    "asyncFunctions",
+    "classConstructorCall",
+    "doExpressions",
+    "trailingFunctionCommas",
+    "objectRestSpread",
+    "decorators-legacy",
+    "classProperties",
+    "exportExtensions",
+    "exportDefaultFrom",
+    "exportNamespaceFrom",
+    "exponentiationOperator",
+    "asyncGenerators",
+    "functionBind",
+    "functionSent",
+    "dynamicImport"
+  ]
 };
